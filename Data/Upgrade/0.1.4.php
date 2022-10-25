938d6e187bef988cf49c28346ca3bd991fdbd8f1
久治明千树汐 <admin@soraharu.com>
2022-10-25 15:07:36 +0800

    chore: remove on_database_upgrade_logs table

--- Data/Upgrade/0.1.4.php
+++ Data/Upgrade/0.1.4.php
@@ -14,6 +14,7 @@ if ($current_version === '0.1.4') {
 	@$database->query('CREATE INDEX <on_categories_id_IDX> ON <on_categories> (<id> ASC)');
 	@$database->query('CREATE INDEX <on_categories_title_IDX> ON <on_categories> (<title>)');
 	@$database->query('CREATE INDEX <on_categories_weight_IDX> ON <on_categories> (<weight> DESC)');
+	@$database->drop('on_database_upgrade_logs');
 
 	$helper->setGlobalConfig_AuthRequired('CONFIG_VERSION', CONFIG_VERSION, '0.1.5');
 	$current_version = '0.1.5';
